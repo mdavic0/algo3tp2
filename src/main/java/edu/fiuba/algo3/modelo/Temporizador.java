d1327aa184b13775a087f211e1bf384cd742efd6
Maferep <mf.pont.tv@gmail.com>
2021-12-01 15:41:26 -0300

    mas tests para sueño

--- src/main/java/edu/fiuba/algo3/modelo/Temporizador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Temporizador.java
@@ -4,6 +4,7 @@ public class Temporizador {
     int hora_inicial;
     int horas_transcurridas;
     int hora_dormir;
+    int horas_de_sueño = 8;
     int hora_limite;
     //TODO: reemplazar con patron de estado
     String estado = "Activo";
@@ -18,12 +19,15 @@ public class Temporizador {
     int horaActual(){
         return (hora_inicial + horas_transcurridas) % 24;
     }
+    int horasTranscurridas(){
+        return horas_transcurridas;
+    }
 
     private String aumentarHoraActual(int hora_a_agregar){
         horas_transcurridas +=hora_a_agregar;
         if(horaActual() >= hora_dormir) {
             //Nota: hacer esto un evento que sea notificado al policia y al UI
-            horas_transcurridas += 8;
+            horas_transcurridas += horas_de_sueño;
         }
         if(horaActual() >= hora_limite) {
             //nota: reemplazar con patron de estado y hacer evento
--- src/main/test/edu/fiuba/algo3/modelo/TemporizadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TemporizadorTest.java
@@ -13,5 +13,20 @@ public class TemporizadorTest {
         int hora_limite = 48;
         Temporizador timer = new Temporizador(hora_inicial, hora_dormir, hora_limite); //hora inicial, hora_dormir, hora_limite
         timer.reportarActividad(new Actividad(5));
+        assertEquals(timer.horaActual(), hora_inicial + 5);
+        assertEquals(timer.horasTranscurridas(), 5);
+    }
+
+    @Test
+    public void testEventoQueDuraHastaLas23CausaSueño() throws Exception { 
+        int hora_inicial = 9;
+        int hora_dormir = 23;
+        int hora_limite = 48;
+        Temporizador timer = new Temporizador(hora_inicial, hora_dormir, hora_limite); //hora inicial, hora_dormir, hora_limite
+        timer.reportarActividad(new Actividad(14));
+
+        assertEquals(23, 9 + 14); //actividad termina a las 23:00 
+        assertEquals(14 + 8, timer.horasTranscurridas());
+        assertEquals(((9 + 14 + 8) % 24), timer.horaActual()); //horaActual() provee el horario entre 0 y 23 horas
     }
 }
