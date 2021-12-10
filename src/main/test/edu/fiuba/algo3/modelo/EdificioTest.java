3e0e7bcd56971965a905c3fd70d914e79a1675aa
Mauricio Davico <mdavico@fi.uba.ar>
2021-12-10 20:47:19 -0300

    limpieza de test Entrega2

--- src/main/test/edu/fiuba/algo3/modelo/EdificioTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/EdificioTest.java
@@ -16,10 +16,8 @@ public class EdificioTest {
         IPais Argentina = new PaisMock(robo.lugarDeRobo().nombre);
         Policia paco = new Policia(Argentina, new Temporizador(0));
 
-        IEdificio aeropuerto = new Edificio (
-            "aeropuerto", Argentina,
-            new NoEstuvoEnEdificio());
-                                                                                                // TIPO DE EDIFICIO
+        IEdificio aeropuerto = new Edificio ("aeropuerto", Argentina, new NoEstuvoEnEdificio());
+
         Argentina.agregarEdificio(aeropuerto);
 
         for (int x = 0; x < cantidadDeVisitasAlAeropuerto; x++){
@@ -31,33 +29,15 @@ public class EdificioTest {
 
     }
 
-    /*
-    Los tests fallan porque no estamos creando un robo mock sino uno real que obtiene un pais
-    aleatoriamente. Entonces para que el test funcione, debemos crear un robo Mock.
-    El robo mock debe implementar pistaParaPais y pistaParaLadron que nos permita inyectar
-    la pista que queremos.
-
-    Ademas, para que los tipos de edificios funcionen, podemos generar los Testigos con una pista
-    generada por el tipo de edificio:
-
-    class EdificioEconomico
-        crearTestigo(robo):
-            return new Testigo(new PistaEconomica(robo))
-    
-    */
     @Test
     public void corroborarPista() throws Exception{
 
         String pistaDeseada = "Lo siento, no he visto a nadie parecido por esta zona...";
         String pistaObtenida;
 
-        IRobo robo = new RoboMock();
         IPais pais = new PaisMock ("Francia");
 
-        IEdificio banco = new Edificio (
-            "banco", 
-            pais,
-            new NoEstuvoEnEdificio());
+        IEdificio banco = new Edificio ("banco", pais, new NoEstuvoEnEdificio());
 
         pistaObtenida = banco.obtenerTestimonio();
 
