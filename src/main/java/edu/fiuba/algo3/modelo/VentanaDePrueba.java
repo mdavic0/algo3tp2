8133fe1e8b2cf2247b04f35e0e27159fab8fae9d
FPomi <pomifranco@hotmail.com>
2021-12-14 21:38:43 -0300

    intento de agregado de librerias

--- src/main/java/edu/fiuba/algo3/modelo/VentanaDePrueba.java
+++ src/main/java/edu/fiuba/algo3/modelo/VentanaDePrueba.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.modelo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,16 +10,18 @@ import java.io.IOException;
 
 public class VentanaDePrueba extends Application {
 
-    public static void main (String[] args){
+   public static void main (String[] args){
         launch(args);
     }
 
     @Override
-    public void star (Stage ventana) throws Exception{
-        Parent raiz = FXMLLoader.load(getClass().getResource("VentanaDeInicio.fxml"));
-        ventana.setTitle("");
-        ventana.setScene(new Scene (raiz, 300, 400));
-        ventana.Show();
+    public void start (Stage ventana) throws Exception{
+        Parent root = FXMLLoader.load("VentanaDeConfirmacion.fxml");
+        ventana.setTitle("A");
+        ventana.setScene(new Scene (root, 600, 500));
+        ventana.show();
     }
 
 }
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.fiuba.algo3 {
     requires transitive javafx.graphics;
     requires javafx.controls;
+    requires javafx.fxml;
     exports edu.fiuba.algo3;
     exports edu.fiuba.algo3.modelo;
 }
