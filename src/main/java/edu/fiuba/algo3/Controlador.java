7c19dcfcddae02a2c79427a40f3b592ac1dd6b5a
FPomi <pomifranco@hotmail.com>
2021-12-16 16:37:21 -0300

    label en la ventana de confirmacion

--- src/main/java/edu/fiuba/algo3/Controlador.java
+++ src/main/java/edu/fiuba/algo3/Controlador.java
@@ -1,13 +1,20 @@
 package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.modelo.Temporizador;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class Controlador {
+
+    @FXML
+    public Label labelResultado;
 
     Temporizador t = new Temporizador(0,0,0);
 
     public void botonSi (){
         System.out.println(t.horaActual());
+        labelResultado.setText(String.valueOf(t.horaActual()));
+
     }
 
     public void botonNo (){
