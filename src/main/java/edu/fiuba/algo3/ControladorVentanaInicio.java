0129e7b1f66b23d4f727ad49a8a71949dab5a923
Maferep <mf.pont.tv@gmail.com>
2021-12-17 14:48:24 -0300

    ingresar nombre por UI

--- src/main/java/edu/fiuba/algo3/ControladorVentanaInicio.java
+++ src/main/java/edu/fiuba/algo3/ControladorVentanaInicio.java
@@ -7,15 +7,32 @@ import edu.fiuba.algo3.modelo.Policia;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class ControladorVentanaInicio  implements Initializable{
+    private String ultimoInput;
 
     @FXML
     public Label textoMaquinaDeEscribir;
+    @FXML
+    public TextField inputJugador;
+    
+    public void handleOnKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER){
+            recibeInputJugador();
+        }
+    } 
 
     public void imprimirTextoPolicia (Policia policia){
-        textoMaquinaDeEscribir.setText("Hola policia. Tu rango es" + policia.imprimirRango() + ".");
+        textoMaquinaDeEscribir.setText("Hola policia. Tu rango es" + policia.imprimirRango() + ". Ingresá tu nombre!");
     }
+
+    public void recibeInputJugador(){
+        ultimoInput = inputJugador.getText();
+        textoMaquinaDeEscribir.setText("Hola, "+ ultimoInput);
+    } 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
