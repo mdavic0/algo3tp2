0c4214af74d2307c282399e694598efc2d8ed14d
Mauricio Davico <mdavico@fi.uba.ar>
2021-12-18 15:53:07 -0300

    Cada ventana previa al juego con interfaz terminada, FALTA UNIRLAS

---
+++ src/main/java/edu/fiuba/algo3/ControladorVentanaAyuda.java
@@ -0,0 +1,36 @@
+package edu.fiuba.algo3;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorVentanaAyuda implements Initializable {
+
+    @FXML
+    public Label comoJugar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comoJugar.setText("\tEl juego en sí es sumamente simple, como agente de la INTERPOL te dejarán en una \"escena del crimen\"" +
+                " y deberás buscar el rastro de Carmen Sandiego desde la ciudad en la que ocurrió hasta encontrar donde se esconde." +
+                " Para ello podrás usar la lupa para interrogar testigos de cada ciudad y reunir pistas." +
+                " Cuando creas saber dónde está Carmen podrás usar el avión para viajar a la siguiente ciudad.\n\n" +
+                "\tMientras vas jugando, Google Earth te dará información interesante sobre los sitios que visitas," +
+                " de forma que aprendas algo mientras juegas.\n\n" +
+                "\tEs bastante simple y entretenido, además de tener la ventaja de estar explorando sitios reales mientras" +
+                " aprendes y te diviertes. En cuanto a juegos educativos se refiere, este viene excelente, no solo para " +
+                "niños sino para adultos. ");
+    }
+
+    public void botonRegresar(){
+        System.out.print("Regresar");
+    }
+
+
+}
--- src/main/java/edu/fiuba/algo3/ControladorVentanaCreditos.java
+++ src/main/java/edu/fiuba/algo3/ControladorVentanaCreditos.java
@@ -1,12 +1,15 @@
 package edu.fiuba.algo3;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControladorVentanaCreditos implements Initializable {
+    @FXML
+    public Label tpAlgoThief;
+
     @FXML
     public Label versionJava;
 
@@ -18,11 +21,13 @@ public class ControladorVentanaCreditos implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        versionJava.setText("Version de Java:\n" + System.getProperty("java.version"));
-        versionJavaFX.setText("Version de JavaFX:\n" + System.getProperty("javafx.version"));
+        tpAlgoThief.setText("TP2 Algoritmos 3: Algo-thief");
+        versionJava.setText("Version de Java:\n\t- " + System.getProperty("java.version"));
+        versionJavaFX.setText("Version de JavaFX:\n\t- " + System.getProperty("javafx.version"));
         desarrolladores.setText("Desarrolladores:\n\n- Mauricio Davico\n" +
                 "- María Fernanda Pont Tovar\n" +
                 "- Franco Pomi");
+
     }
 
     public void botonRegresar(){
