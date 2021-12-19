611e9e8a31341dc036ff5519d48a47d1926e7b2c
Maferep <mf.pont.tv@gmail.com>
2021-12-19 14:40:13 -0300

    Eliminado codigo no utilizado en ControladorVentanaBienvenida

--- src/main/java/edu/fiuba/algo3/ControladorVentadaBienvenida.java
+++ src/main/java/edu/fiuba/algo3/ControladorVentadaBienvenida.java
@@ -3,23 +3,14 @@ package edu.fiuba.algo3;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class ControladorVentadaBienvenida implements Initializable {
+public class ControladorVentadaBienvenida {
     @FXML
     public AnchorPane raiz;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
 
     public void botonJugar() throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("VentanaDeInicio" + ".fxml"));
